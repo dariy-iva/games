8d2e3bd08aea6d9201e752cac0617fd33ffcf70b
Daria Ivanova <dashann@mail.ru>
2022-01-25 23:19:16 +0300

    feat/add hover eff

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,72 +5,56 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header__menu">
-      <a href="/">
-        <img src={logo} alt="logo" className="header__logo link-hover" />
-      </a>
-      <a
-        href="/"
-        className="menu__text menu__text_weight_bolder link-hover"
-      >
-        partie
-      </a>
-      <nav className="menu">
-        <ul className="menu__list menu__list_content_nav">
-          <li className="menu__item">
-            <a
-              href="/"
-              className="menu__text menu__text_weight_normal link-hover"
-            >
-              Features
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              href="/"
-              className="menu__text menu__text_weight_normal link-hover"
-            >
-              Partners
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              href="/"
-              className="menu__text menu__text_weight_normal link-hover"
-            >
-              Contact
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              href="/"
-              className="menu__text menu__text_weight_normal link-hover"
-            >
-              Help &amp; Support
-            </a>
-          </li>
-        </ul>
-        <ul className="menu__list menu__list_content_auth">
-          <li className="menu__item">
-            <a
-              href="/"
-              className="menu__text menu__text_weight_bold link-hover"
-            >
-              Log In
-            </a>
-          </li>
-          <li className="menu__text menu__item menu__item_active">
-            <a
-              href="/"
-              className="menu__text menu__text_weight_bold link-hover"
-            >
-              Sign Up
-            </a>
-          </li>
-        </ul>
-      </nav>
-      </div>      
-      <h1 className="header__title translation">More Than Matchmaking</h1>
-      <button type="button" className="header__button translation"></button>
+        <a href="/">
+          <img src={logo} alt="logo" className="header__logo" />
+        </a>
+        <a href="/" className="menu__text menu__text_weight_bolder">
+          partie
+        </a>
+        <nav className="menu">
+          <ul className="menu__list menu__list_content_nav">
+            <li className="menu__item">
+              <a href="/" className="menu__text menu__text_weight_normal">
+                Features
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="/" className="menu__text menu__text_weight_normal">
+                Partners
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="/" className="menu__text menu__text_weight_normal">
+                Contact
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="/" className="menu__text menu__text_weight_normal">
+                Help &amp; Support
+              </a>
+            </li>
+          </ul>
+          <ul className="menu__list menu__list_content_auth">
+            <li className="menu__item">
+              <a href="/" className="menu__text menu__text_weight_bold">
+                Log In
+              </a>
+            </li>
+            <li className="menu__text menu__item">
+              <a
+                href="/"
+                className="menu__text menu__text_weight_bold menu__item_active"
+              >
+                Sign Up
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="header__content">
+        <h1 className="header__title translation">More Than Matchmaking</h1>
+        <button type="button" className="header__button translation"></button>
+      </div>
     </header>
   );
 }
