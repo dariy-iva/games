1809a59860bd91a53f4f2c32805f13c144a8599c
Pavel.A.Ivanov <Pavel.A.Ivanov@wifire.ru>
2022-05-18 21:34:47 +0300

    refactor/structure of files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,52 +1,24 @@
 import React from "react";
 import './Header.css';
 import logo from "../../images/logo.svg";
-import {menuItems} from '../../utils/constants.js'
+import Menu from "../Menu/Menu.js";
 
 export default function Header() {
   return (
-    <>
       <header className="header">
         <div className="header__menu">
           <a href="/">
-            <img src={logo} alt="logo" className="header__logo" />
+            <img src={logo} alt="logo" className="header__logo"/>
           </a>
-          <a href="/" className="menu__text menu__text_weight_bolder">
+          <a href="/" className="header__text">
             partie
           </a>
-          <nav className="menu">
-            <ul className="menu__list menu__list_content_nav">
-              {menuItems.map(item => (
-                 <li className="menu__item">
-                   <a href={item.link} key={item.text} className="menu__text menu__text_weight_normal">
-                     {item.text}
-                   </a>
-                 </li>
-              ))}
-            </ul>
-            <ul className="menu__list menu__list_content_auth">
-              <li className="menu__item">
-                <a href="/" className="menu__text menu__text_weight_bold">
-                  Log In
-                </a>
-              </li>
-              <li className="menu__text menu__item">
-                <a
-                  href="/"
-                  className="menu__text menu__text_weight_bold menu__item_active"
-                >
-                  Sign Up
-                </a>
-              </li>
-            </ul>
-          </nav>
+          <Menu/>
         </div>
         <div className="header__content">
           <h1 className="header__title translation">More Than Matchmaking</h1>
           <button type="button" className="header__button translation"></button>
         </div>
       </header>
-      
-    </>
   );
 }
