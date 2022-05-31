7b0360a4be34764401ef461a577bd9a9e917e336
Daria Ivanova <dashann@mail.ru>
2022-05-31 23:09:43 +0300

    feat/style for Select component in FormSign

--- src/components/FormSign/SelectFormSign/SelectFormSign.js
+++ src/components/FormSign/SelectFormSign/SelectFormSign.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./SelectFormSign.css";
 
 export default function SelectFormSign({value, onChange, config, error, pattern}) {
-  const { label, type, minLength = null, maxLength = null, name, placeholder, options } = config;
+  const {label, type, minLength = null, maxLength = null, name, placeholder, options} = config;
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -26,29 +26,31 @@ export default function SelectFormSign({value, onChange, config, error, pattern}
 //   reset.addEventListener('click', () => {
 //     selectSingle_title.textContent = selectSingle_title.getAttribute('data-default');
 //   });
-  const [state, setState] = React.useState('');
-  const [currentValueText, setCurrentValueText] = React.useState(label);
+  const [state, setState] = React.useState( '' );
+  const [currentValueText, setCurrentValueText] = React.useState( label );
 
   function handleSelectClick() {
-    state === 'active' ? setState('') : setState('active');
+    state === 'active' ? setState( '' ) : setState( 'active' );
   }
+
   function handleLabelClick(e) {
-    setCurrentValueText(e.target.textContent);
-    setState('');
+    setCurrentValueText( e.target.textContent );
+    setState( '' );
   }
 
 
   return (
     <>
-      <div className="select" data-state={state}>
-        <div className={`select__title ${state === 'active' ? 'select__title_active' : ''}`} data-default="Option 0" onClick={handleSelectClick}>{currentValueText}</div>
+      <div className={`select ${name === 'month' ? 'select_name_month' : 'select_name_day-year'}`} data-state={state}>
+        <div className={`select__title ${state === 'active' ? 'select__title_active' : ''}`} data-default="Option 0"
+             onClick={handleSelectClick}>{currentValueText}</div>
         <div className="select__options">
-          {options.map((option, index) => (
+          {options.map( (option, index) => (
             <div key={option}>
               <input id={`name-${index}`} className="select__option" type="radio" name={name}/>
               <label htmlFor={`name-${index}`} className="select__label" onClick={handleLabelClick}>{option}</label>
             </div>
-          ))}
+          ) )}
         </div>
       </div>
     </>
