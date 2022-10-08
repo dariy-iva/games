d6bc401996e537d7366ea9a25ce656e96586358a
Дарья Иванова <dashann@mail.ru>
2022-10-08 19:10:01 +0300

    feat/add styles in main page for adaptive

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,17 +1,30 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 import './MainPage.css';
 import HeaderMain from "../Headers/HeaderMain/HeaderMain.js";
 import Menu from "../Menu/Menu.js";
 import MainVideo from "../MainVideo/MainVideo";
 import TokenAuthButtons from "../TokenAuthButtons/TokenAuthButtons";
+import Button from "../Button/Button";
 import AnimationOverlay from "../AnimationOverlay/AnimationOverlay.js";
+import {pathsConfig} from "../../utils/constants/pathList.js";
 
 export default function MainPage({onLogin}) {
+  const history = useNavigate();
+
   const colorsForOverlay = ["#6E4AFF", "#9C42AB"];
   const [videoIsOpen, setVideoIsOpen] = React.useState(false);
 
   function handleOpenVideo() {
     setVideoIsOpen(true);
+  }
+
+  function handleRegisterButtonClick() {
+    history( pathsConfig.register );
+  }
+
+  function handleLoginButtonClick() {
+    history( pathsConfig.login );
   }
 
   return (
@@ -24,6 +37,8 @@ export default function MainPage({onLogin}) {
         <button type="button" className={`splash-screen__button translationDownToCenter neon-box-shadow ${videoIsOpen ? 'translationCenterToUp' : ''}`} onClick={handleOpenVideo}/>
         {videoIsOpen && <MainVideo />}
         <div className="splash-screen__container">
+          <Button type="button" text="Sign up to Games" disabled={false} onClick={handleRegisterButtonClick} className="splash-screen__register" />
+          <Button type="button" text="Log in" disabled={false} onClick={handleLoginButtonClick} className="splash-screen__login" />
           <TokenAuthButtons onLogin={onLogin} />
         </div>
       </main>
