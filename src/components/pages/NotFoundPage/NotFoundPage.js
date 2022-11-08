41737f0a49fc548e6ab18b8ba333ff8e1f30f737
Дарья Иванова <dashann@mail.ru>
2022-11-08 16:47:53 +0300

    refactor/change tag for redirect in not found page

--- src/components/pages/NotFoundPage/NotFoundPage.js
+++ src/components/pages/NotFoundPage/NotFoundPage.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./NotFoundPage.css";
-import {pathsConfig} from "../../../utils/constants/pathList";
 
 export default function NotFoundPage() {
+  const history = useNavigate();
+
+  function handleBackClick() {
+    history(-1);
+  }
+
   return (
     <div className="not-found">
       <h2 className="not-found__title">404</h2>
       <p className="not-found__text">
         Oops! Page not found
       </p>
-      <Link className="not-found__link-redirect" to={pathsConfig.main}>
+      <button type="button" className="not-found__button-redirect" onClick={handleBackClick}>
         Back to the fun
-      </Link>
+      </button>
     </div>
   );
 }
